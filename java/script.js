850bebc891e23e98f001061478b5c0fe4a4baaa4
kenojason <85342985+kenojason@users.noreply.github.com>
2021-06-25 03:47:53 -0700

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,41 @@
+function myFunction()
+{
+    document.getElementById('demo').innerHTML = "The paragraph has changed"
+}
+document.write("java is interesting");
+
+//window.alert("frontend")
+
+console.log(900)
+ 
+var x = 5;
+var y = 6;
+var y = 15;
+var z = x + y;
+console.log(z);
+
+var name = 'emeka'; //string data type
+var age = 30;
+var rightWrong = true;
+var numChildren = null;
+
+var food = 'rice';
+var color = 'red';
+var decision = 'good';
+var numberDrinks = 'null';
+var music = 'undefined';
+
+//console log
+
+console.log(food);
+console.log(color);
+console.log(decision);
+console.log(numberDrinks);
+console.log(music);
+
+document.getElementById('food').innerHTML = food
+document.getElementById('color').innerHTML = color
+document.getElementById('decision').innerHTML = decision
+document.getElementById('numberDrinks').innerHTML = numberDrinks
+document.getElementById('music').innerHTML = music
+
